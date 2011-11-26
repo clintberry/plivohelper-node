f5aa44d45c827fa41cd628c5725a75633904bfa9
Clint Berry <clint@elojasystems.com>
2011-11-25 23:45:48 -0700

    Initial commit - Skeleton functions created for Rest API

---
+++ plivo.js
@@ -0,0 +1,145 @@
+
+
+
+Plivo = {};
+
+Plivo.options = {};
+Plivo.options.host = 'http://localhost';
+Plivo.options.port = '8080';
+Plivo.options.version = 'v1';
+Plivo.options.accountSid = '';
+Plivo.options.authToken = '';
+
+
+Plivo.request = function (path, method, vars) {
+    
+};
+
+Plivo.phoneStatus = function (vars, callback) {
+    
+};
+
+Plivo.phoneSearch = function (vars, callback) {
+    
+};
+
+Plivo.phoneOrder = function (vars, callback) {
+    
+};
+
+Plivo.call = function (vars, callback) {
+    
+};
+
+Plivo.bulkCall = function (vars, callback) {
+    
+};
+
+Plivo.groupCall = function (vars, callback) {
+    
+};
+
+Plivo.transferCall = function (vars, callback) {
+    
+};
+
+Plivo.hangupAllCalls = function (callback) {
+    
+};
+
+Plivo.hangupCall = function (vars, callback) {
+    
+};
+
+Plivo.scheduleHangup = function (vars, callback) {
+    
+};
+
+Plivo.cancelScheduledHangup = function (vars, callback) {
+    
+};
+
+Plivo.recordStart = function (vars, callback) {
+    
+};
+
+Plivo.recordStop = function (vars, callback) {
+    
+};
+
+Plivo.play = function (vars, callback) {
+    
+};
+
+Plivo.playStop = function (vars, callback) {
+    
+};
+
+Plivo.schedulePlay = function (vars, callback) {
+    
+};
+
+Plivo.cancelScheduledPlay = function (vars, callback) {
+    
+};
+
+Plivo.sendDigits = function (vars, callback) {
+    
+};
+
+Plivo.conferenceMute = function (vars, callback) {
+    
+};
+
+Plivo.conferenceUnmute = function (vars, callback) {
+    
+};
+
+Plivo.conferenceKick = function (vars, callback) {
+    
+};
+
+Plivo.conferenceHangup = function (vars, callback) {
+    
+};
+
+Plivo.conferenceDeaf = function (vars, callback) {
+    
+};
+
+Plivo.conferenceUndeaf = function (vars, callback) {
+    
+};
+
+Plivo.conferenceRecordStart = function (vars, callback) {
+    
+};
+
+Plivo.conferenceRecordStop = function (vars, callback) {
+    
+};
+
+Plivo.conferencePlay = function (vars, callback) {
+    
+};
+
+Plivo.conferenceSpeak = function (vars, callback) {
+    
+};
+
+Plivo.conferenceList = function (vars, callback) {
+    
+};
+
+Plivo.conferenceListMembers = function (vars, callback) {
+    
+};
+
+
+//TODO: Program XML response functions so that they can be chained
+Plivo.response = {};
+
+
+
+
+
